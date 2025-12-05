0783700df65c8b5a2d46dbd3c48c12b494c17015
dmstn <eunsoo4629@naver.com>
2025-12-05 18:28:12 +0900

    251205 수업

---
+++ src/main/java/ch06/MemberTable.java
@@ -0,0 +1,52 @@
+package ch06;
+
+import ch06.timer.TimeClient;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class MemberTable {
+    //선언부
+    //JFrame은 디폴트 레이아웃이 BorderLayout - 동,서,남,북,중앙
+    JFrame jf = new JFrame();
+    JPanel jp = new JPanel(); //FlowLayout
+    String [][] data = new String[0][3];
+    String [] cols = new String[]{"아이디","이름","나이"};
+    DefaultTableModel model = new DefaultTableModel(data,cols);
+    JTable jt = new JTable(model);
+    JScrollPane jsp =
+            new JScrollPane(jt,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS
+                    ,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+    JButton btn_select = new JButton("조회");
+    JButton btn_insert = new JButton("입력");
+    JButton btn_update = new JButton("수정");
+    JButton btn_delete = new JButton("삭제");
+    JLabel jlb_time = new JLabel("현재시간출력",JLabel.RIGHT);
+    //생성자
+    public MemberTable(){
+        initDisplay();
+    }
+    //화면처리부
+    public void initDisplay(){
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jp.setBackground(Color.orange);
+        jp.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        jp.add(btn_select);
+        jp.add(btn_insert);
+        jp.add(btn_update);
+        jp.add(btn_delete);
+        TimeClient tc = new TimeClient(jlb_time);
+        tc.start(); //run()직접 호출하는게 아니라 start()하면 run()호출됨.
+        jf.setTitle("회원관리");
+        jf.add("North", jp);
+        jf.add("Center", jsp);
+        jf.add("South", jlb_time);
+        jf.setSize(400, 400);
+        jf.setVisible(true);
+    }
+    //main
+    public static void main(String[] args) {
+        MemberTable mt = new MemberTable();
+    }
+}
