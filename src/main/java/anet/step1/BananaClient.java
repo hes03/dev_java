1be0fb5ef8788ca63de687c85559ae034edc8e13
dmstn <eunsoo4629@naver.com>
2025-12-09 18:23:14 +0900

    251209 수업-채팅클래스/전송/종료 구현

--- src/main/java/anet/step1/BananaClient.java
+++ src/main/java/anet/step1/BananaClient.java
@@ -1,8 +1,8 @@
 package anet.step1;
-//1-1 학습목표
-//클래스 나누어 처리해보기
-//이때 static사용하지 않고 생성자만으로 처리해보기
-//클래스와 클래스 사이에 있는 의존관계를 해결할 수 있다.
+//1-1학습목표
+//클래스 나누어 처리해 보기
+//이때 static사용하지 않고 생성자만으로 처리해 보기
+//클래스와 클래스 사이에는 의존관계가 있다. - 해결할 수 있다.
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -13,11 +13,11 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /*
-JTextField에 사용자가 메시지를 입력하고 엔터(전송-이벤트)하면 처리할 수 있는
+JTextField에 사용자가 메시지를 입력하고 엔터(전송-이벤트)하면 처리할 수  있는
 콜백메서드를 ActionListener가 선언하고 있다.
 화면을 출력하는 것에는 지연이 발생하지 않는데
-소켓을 생성하고 서버로부터 응답을 받아오는데는 waiting이 발생함
-두 가지를 나눈어서 처리하자(우선 순위가 있다)
+소켓을 생성하고 서버로 부터 응답을 받아오는데는 waiting이 발생함.
+두 가지를 나누어서 처리하자(우선 순위가 있다.)
  */
 public class BananaClient extends JFrame implements ActionListener {
     //아래 변수들은 BananaClientThread에서도 필요할 수 있으므로 전변으로 함
@@ -49,30 +49,59 @@ public class BananaClient extends JFrame implements ActionListener {
     JButton jbtn_logout 	= new JButton("로그아웃");
     JButton jbtn_emoticon 	= new JButton("이모티콘");
     JButton jbtn_exit 		= new JButton("종료");
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+        String msg = jtf_msg.getText();
+        //전송 버튼을 눌렀을 때 와 메시지 입력 후 엔터 쳤을 때
+        if(obj == jtf_msg || obj == jbtn_send){
+            if(msg == null || msg.length() == 0){
+                JOptionPane.showMessageDialog(this,"메시지를 입력하세요");
+                return;//actionPerformed탈출함.
+            }
+            try {
+                oos.writeObject(200+"#"+nickName+"#"+msg);
+                jtf_msg.setText("");
+            }catch(Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }//end of 엔터쳤을 때와 전송버튼 클릭했을 때
+        else if(obj == jbtn_exit){
+            try{
+                oos.writeObject(500+"#"+nickName);
+                System.exit(0);//JVM와 연결을 끊음- 스레드 회수, 어플리케이션 종료
+            }catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }//end of 나가기
+    }//end of actionPerformed
 
-    }
-
-    //통신을 위해 필요한 코드 작성하기 - 지연이 발생할 수 있음
+    //통신을 위해 필요한 코드 작성하기 - 지연이 발생할 수 있음.
+    //화면 다음에 순서(차례-Thread)를 가져야 한다.
     public void init(){
         //지연이 발생할 가능성이 있는 코드들이 있다.
-        //주의: 절대로 화면그리기 보다 먼저 호출하지 않기
+        //주의 : 절대로 화면그리기 보다 먼저 호출하지 않기
         try{
             //예외가 발생할 가능성이 있는 코드
-            //소켓 객체 인스턴스화 하기(서버에 accept 당함)
-            socket = new Socket("127.0.0.1", 5000);
+            //소켓 객체 인스턴스화 하기(서버에 accept당함)
+            socket = new Socket("127.0.0.1",5000);
             oos = new ObjectOutputStream(socket.getOutputStream());
             ois = new ObjectInputStream(socket.getInputStream());
             //서버에게 내가 입장한 사실을 알림
             oos.writeObject(100+"#"+nickName);
+            ///서버에서 한 말을 듣기 위한 준비
+            BananaClientThread bct = new BananaClientThread(this);
+            bct.start();
         }catch (Exception e){
-
+            System.out.println(e.getMessage());
         }
     }//end of init
     public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
         BananaClient bc = new BananaClient();
-        //메서드 호출순서를 반드시 지킬것. 왜냐하면 init()먼저 하면 지연이 발생함
+        //메서드 호출 순서를 반드시 지킬 것. 왜냐하면 init()먼저 하면 지연이 발생함.
         bc.initDisplay();
         bc.init();
     }
@@ -86,13 +115,26 @@ public class BananaClient extends JFrame implements ActionListener {
         jtf_msg.addActionListener(this);
         jbtn_send.addActionListener(this);
         jbtn_exit.addActionListener(this);
+        jbtn_one.setBackground(new Color(158,9,9));
+        jbtn_one.setForeground(new Color(212,212,212));
+        jbtn_change.setBackground(new Color(7,84,170));
+        jbtn_change.setForeground(new Color(212,212,212));
+        jbtn_color.setBackground(new Color(19, 99, 57));
+        jbtn_color.setForeground(new Color(212, 212, 212));
+        jbtn_emoticon.setBackground(new Color(121, 85, 72));   // 모던 초콜릿 브라운
+        jbtn_emoticon.setForeground(new Color(255, 248, 240)); // 크림 화이트
+        jbtn_logout.setBackground(new Color(255, 255, 200));
+        jbtn_logout.setForeground(new Color(57, 57, 57));
+        jbtn_exit.setBackground(new Color(54, 54, 54));
+        jbtn_exit.setForeground(new Color(212, 212, 212));
+
         //사용자로 부터 닉네임 입력 받기
         //nickName = JOptionPane.showInputDialog("닉네임을 입력하세요.");
         this.setLayout(new GridLayout(1,2,2,2));
         //JFrame에 들어갈 첫번째 속지는 동서남북 중앙으로 배치할 수 있어야 해요.
         jp_first.setBackground(Color.orange);
         jp_first.setLayout(new BorderLayout());
-        jp_first.add("Center",jta_display);
+        jp_first.add("Center",jsp_display);
         jp_first.add("South",jp_first_south);
         jp_first_south.setLayout(new BorderLayout());
         jp_first_south.add("Center",jtf_msg);
@@ -116,6 +158,6 @@ public class BananaClient extends JFrame implements ActionListener {
         this.setTitle(nickName+"님의 대화창");
         this.setSize(800, 550);
         this.setVisible(true);
+
     }//end of initDisplay
 }
-
